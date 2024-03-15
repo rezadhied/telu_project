feb068c9b28e60493a55386fa32d8d75a65299bd
mzaky <mzakyf5503@gmail.com>
2024-03-15 10:55:06 +0700

    menambahkan color aplikasi

---
+++ lib/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFFEFBF6);
+  static const Color black = Color(0xFF3D3C42);
+  static const Color grey = Color(0XFFD9D9D9);
+  static const Color primary = Color(0xFFD31C1C);
+  static const Color secondary = Color(0xFF77D31C);
+  static const Color tertiary = Color(0xFF1CD3D3);
+  static const Color quarternary = Color(0xFF771CD3);
+  static const Color blackAlternative = Color(0xFF252422);
+  static const Color primaryAlternative = Color(0xFFE53D3D);
+  static const Color secondaryAlternative = Color(0xFF91E53D);
+  static const Color tertiaryAlternative = Color(0xFF3DE5E5);
+  static const Color quarternaryAlternative = Color(0xFF913DE5);
+}
