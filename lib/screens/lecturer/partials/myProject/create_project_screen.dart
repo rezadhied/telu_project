0552e5020cffbb0fa08a342f2f91691a429ef3e4
mzaky <mzakyf5503@gmail.com>
2024-05-19 23:54:56 +0700

    chore: Update navigation after successful project creation

--- lib/screens/lecturer/partials/myProject/create_project_screen.dart
+++ lib/screens/lecturer/partials/myProject/create_project_screen.dart
@@ -93,15 +93,14 @@ class _CreateProjectPageState extends State<CreateProjectPage> {
         );
 
         if (response.statusCode == 201) {
-          // Navigator.push(
-          //   context,
-          //   MaterialPageRoute(
-          //     builder: (context) => const AppNavigationBar(
-          //       isStudent: false,
-          //       selectedIndex: 1,
-          //     ),
-          //   ),
-          // );
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const MainApp(
+                selectedIndex: 1,
+              ),
+            ),
+          );
         } else {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text('Failed to create project.')),
