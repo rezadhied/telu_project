6aeb1f901e35d4a43560327574f0c1a5469646d4
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2024-05-28 08:42:31 +0700

    test

--- lib/screens/lecturer/profile_lecturer.dart
+++ lib/screens/lecturer/profile_lecturer.dart
@@ -52,10 +52,11 @@ class _ProfileLecturerState extends State<ProfileLecturer> {
     }
   }
 
-  Future<dynamic> fetchUserData() async {
+  Future<Map<String, dynamic>> fetchUserData() async {
     String url = Provider.of<ApiUrlProvider>(context, listen: false).baseUrl;
     SharedPreferences pref = await SharedPreferences.getInstance();
     String userId = pref.getString('userId') ?? '';
+    print(userId);
     final response = await http.get(Uri.parse('$url/users/$userId'));
     print(json.decode(response.body));
     if (response.statusCode == 200) {
@@ -71,158 +72,166 @@ class _ProfileLecturerState extends State<ProfileLecturer> {
       body: FutureBuilder(
           future: fetchUserData(),
           builder: (context, snapshot) {
-            return Container(
-              color: AppColors.white,
-              padding: const EdgeInsets.only(top: 1.0),
-              child: Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.only(bottom: 24),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            bottom:
-                                BorderSide(width: 1, color: Colors.black26))),
-                    child: Column(
-                      children: [
-                        GestureDetector(
-                          onTap: isEditing ? _pickImage : null,
-                          child: Stack(
-                            alignment: Alignment.center,
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              var userdata = snapshot.data!;
+              return Container(
+                color: AppColors.white,
+                padding: const EdgeInsets.only(top: 1.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.only(bottom: 24),
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom:
+                                  BorderSide(width: 1, color: Colors.black26))),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap: isEditing ? _pickImage : null,
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: isEditing
+                                        ? Border.all(
+                                            color: Colors.red, width: 2.0)
+                                        : null,
+                                  ),
+                                  child: CircleAvatar(
+                                    radius: 50,
+                                    backgroundColor: Colors.grey,
+                                    backgroundImage: imagePath != null
+                                        ? FileImage(File(imagePath!))
+                                        : AssetImage(imagePath!)
+                                            as ImageProvider,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: isEditing
-                                      ? Border.all(
-                                          color: Colors.red, width: 2.0)
-                                      : null,
-                                ),
-                                child: CircleAvatar(
-                                  radius: 50,
-                                  backgroundColor: Colors.grey,
-                                  backgroundImage: imagePath != null
-                                      ? FileImage(File(imagePath!))
-                                      : AssetImage(imagePath!) as ImageProvider,
-                                ),
+                              Text(
+                                userdata['firstName'],
+                                style: TextStyle(
+                                    fontSize: 24, fontWeight: FontWeight.bold),
                               ),
                             ],
                           ),
-                        ),
-                        const SizedBox(height: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              snapshot.data["firstName"]!,
-                              style: TextStyle(
-                                  fontSize: 24, fontWeight: FontWeight.bold),
-                            ),
-                            Opacity(
-                              opacity: 0.5,
-                              child: Text('j.smith@example.com'),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Visibility(
-                              visible: !isEditing,
-                              child: Row(
-                                children: [
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isEditing = !isEditing;
-                                      });
-                                    },
-                                    child: const Text(
-                                      'Edit',
-                                      style: TextStyle(color: Colors.red),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 24),
-                                  ElevatedButton(
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Visibility(
+                                visible: !isEditing,
+                                child: Row(
+                                  children: [
+                                    ElevatedButton(
                                       onPressed: () {
-                                        Provider.of<AuthProvider>(context,
-                                                listen: false)
-                                            .logoutUser();
-                                        Navigator.of(context).push(
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    WelcomePage()));
+                                        setState(() {
+                                          isEditing = !isEditing;
+                                        });
                                       },
-                                      child: Text("Logout")),
-                                ],
-                              ),
-                            ),
-                            if (isEditing)
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isEditing = false;
-                                      });
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      foregroundColor: Colors.white,
-                                      backgroundColor: Colors.green,
+                                      child: const Text(
+                                        'Edit',
+                                        style: TextStyle(color: Colors.red),
+                                      ),
                                     ),
-                                    child: const Text('Save'),
-                                  ),
-                                  const SizedBox(width: 24),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isEditing = false;
-                                      });
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      foregroundColor: Colors.white,
-                                      backgroundColor: Colors.red,
-                                    ),
-                                    child: const Text('Cancel'),
-                                  ),
-                                ],
+                                    const SizedBox(width: 24),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          Provider.of<AuthProvider>(context,
+                                                  listen: false)
+                                              .logoutUser();
+                                          Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      WelcomePage()));
+                                        },
+                                        child: Text("Logout")),
+                                  ],
+                                ),
                               ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 24),
-                          buildTextFormField('First Name', firstNameController),
-                          const SizedBox(height: 16),
-                          buildTextFormField('Last Name', lastNameController),
-                          const SizedBox(height: 16),
-                          buildTextFormField('NIM', nimController),
-                          const SizedBox(height: 16),
-                          buildTextFormField('No Handphone', phoneController),
-                          const SizedBox(height: 16),
-                          buildTextFormField('Gender', genderController),
-                          const SizedBox(height: 16),
-                          buildTextFormField('Faculty', facultyController),
-                          const SizedBox(height: 16),
-                          buildTextFormField('Major', majorController),
-                          const SizedBox(height: 16),
+                              if (isEditing)
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isEditing = false;
+                                        });
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        foregroundColor: Colors.white,
+                                        backgroundColor: Colors.green,
+                                      ),
+                                      child: const Text('Save'),
+                                    ),
+                                    const SizedBox(width: 24),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isEditing = false;
+                                        });
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        foregroundColor: Colors.white,
+                                        backgroundColor: Colors.red,
+                                      ),
+                                      child: const Text('Cancel'),
+                                    ),
+                                  ],
+                                ),
+                            ],
+                          )
                         ],
                       ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          }),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 24),
+                            buildTextFormField(
+                                'First Name', firstNameController),
+                            const SizedBox(height: 16),
+                            buildTextFormField('Last Name', lastNameController),
+                            const SizedBox(height: 16),
+                            buildTextFormField('NIM', nimController),
+                            const SizedBox(height: 16),
+                            buildTextFormField('No Handphone', phoneController),
+                            const SizedBox(height: 16),
+                            buildTextFormField('Gender', genderController),
+                            const SizedBox(height: 16),
+                            buildTextFormField('Faculty', facultyController),
+                            const SizedBox(height: 16),
+                            buildTextFormField('Major', majorController),
+                            const SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+          }
+
+          // var userData = snapshot.data!;
+          ),
     );
   }
 
