8947f3195b4b695ec49811e0ac4a814bc995e0fe
mzaky <mzakyf5503@gmail.com>
2024-06-09 16:59:44 +0700

    fix bug loading requested lecture

--- lib/screens/lecturer/inbox_lecturer.dart
+++ lib/screens/lecturer/inbox_lecturer.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -88,29 +89,8 @@ class Request {
 }
 
 class _InboxLecturerState extends State<InboxLecturer> {
-  Future<void> fetchRequests() async {
-    String url = Provider.of<ApiUrlProvider>(context, listen: false).baseUrl;
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    String userId = pref.getString('userId') ?? '';
-    //String userId = '1307684006';
-    final response = await http.get(Uri.parse('$url/requestMember/$userId'));
-    //final response = await http.get(Uri.parse('http://10.0.2.2:5000/requestMember/$userId'));
-    if (response.statusCode == 200) {
-      final List projectsJson = json.decode(response.body);
-      if (mounted) {
-        setState(() {
-          originalRequests = projectsJson.expand((projectJson) {
-            return (projectJson['Requests'] as List).map((requestJson) {
-              return Request.fromJson({...requestJson, 'project': projectJson});
-            });
-          }).toList();
-          filteredRequests = originalRequests;
-        });
-      }
-    } else {
-      throw Exception('Failed to load requests');
-    }
-  }
+  bool isLoading = false;
+  bool isLoadingConfirmation = false;
 
   void filterRequest(String query) {
     if (mounted) {
@@ -130,6 +110,45 @@ class _InboxLecturerState extends State<InboxLecturer> {
     }
   }
 
+  Future<void> fetchRequests() async {
+    if (!mounted) return;
+    setState(() {
+      filteredRequests = [];
+      originalRequests = [];
+      isLoading = true;
+    });
+
+    String url = Provider.of<ApiUrlProvider>(context, listen: false).baseUrl;
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    String userId = pref.getString('userId') ?? '';
+    final response = await http.get(Uri.parse('$url/requestMember/$userId'));
+
+    if (!mounted) return;
+
+    if (response.statusCode == 200) {
+      final List projectsJson = json.decode(response.body);
+
+      if (mounted) {
+        setState(() {
+          originalRequests = projectsJson.expand((projectJson) {
+            return (projectJson['Requests'] as List).map((requestJson) {
+              return Request.fromJson({...requestJson, 'project': projectJson});
+            });
+          }).toList();
+          filteredRequests = originalRequests;
+          isLoading = false;
+        });
+      }
+    } else {
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+      throw Exception('Failed to load requests');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -142,132 +161,150 @@ class _InboxLecturerState extends State<InboxLecturer> {
       backgroundColor: AppColors.white,
       body: RefreshIndicator(
         onRefresh: fetchRequests,
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              automaticallyImplyLeading: false,
-              title: Container(
-                padding: const EdgeInsets.fromLTRB(0, 20, 10, 0),
-                margin: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  'Requested',
-                  style: GoogleFonts.inter(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w900,
-                    color: AppColors.primary,
+        child: Stack(
+          children: [
+            CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  automaticallyImplyLeading: false,
+                  title: Container(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 10, 0),
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: Text(
+                      'Requested',
+                      style: GoogleFonts.inter(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ),
+                  backgroundColor: AppColors.white,
+                  floating: false,
+                  pinned: false,
+                  elevation: 0,
+                ),
+                SliverToBoxAdapter(
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(15, 15, 15, 15),
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Find Request',
+                            style: GoogleFonts.inter(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 5),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            border: Border.all(
+                                color: AppColors.black.withOpacity(0.30)),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Search',
+                              hintStyle: GoogleFonts.inter(
+                                fontSize: 14,
+                                color: AppColors.black,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                            onChanged: (value) {
+                              filterRequest(value);
+                            },
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 20),
+                          child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "${filteredRequests.length} ",
+                                    style: GoogleFonts.inter(
+                                        fontSize: 14,
+                                        color: AppColors.black,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                  Text(
+                                    'Request',
+                                    style: GoogleFonts.inter(
+                                        fontSize: 14, color: AppColors.black),
+                                  ),
+                                ],
+                              )),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              backgroundColor: AppColors.white,
-              floating: true,
-              pinned: true,
-              elevation: 0,
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                margin: const EdgeInsets.fromLTRB(15, 15, 15, 15),
-                padding: const EdgeInsets.symmetric(horizontal: 5),
-                child: Column(
-                  children: [
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Find Request',
-                        style: GoogleFonts.inter(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 5),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(14),
-                        border: Border.all(
-                            color: AppColors.black.withOpacity(0.30)),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search',
-                          hintStyle: GoogleFonts.inter(
-                            fontSize: 14,
-                            color: AppColors.black,
+                filteredRequests.isEmpty && !isLoading
+                    ? SliverFillRemaining(
+                        child: Center(
+                          child: Text(
+                            'Belum ada Request',
+                            style: GoogleFonts.inter(
+                              fontSize: 14,
+                              color: AppColors.black,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
-                          border: InputBorder.none,
                         ),
-                        onChanged: (value) {
-                          if (mounted) {
-                            setState(() {
-                              filterRequest(value);
-                            });
-                          }
-                        },
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 20),
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Row(
-                            children: [
-                              Text(
-                                "${filteredRequests.length} ",
-                                style: GoogleFonts.inter(
-                                    fontSize: 14,
-                                    color: AppColors.black,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                              Text(
-                                'Request',
-                                style: GoogleFonts.inter(
-                                    fontSize: 14, color: AppColors.black),
-                              ),
-                            ],
-                          )),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            filteredRequests.isEmpty
-                ? SliverFillRemaining(
-                    child: Center(
-                      child: Text(
-                        'Belum ada Request',
-                        style: GoogleFonts.inter(
-                          fontSize: 14,
-                          color: AppColors.black,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                  )
-                : SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => RequestDetail(
-                                  request: filteredRequests[index],
-                                ),
+                      )
+                    : SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => RequestDetail(
+                                      request: filteredRequests[index],
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: RequestItem(
+                                request: filteredRequests[index],
+                                onRequestUpdated: fetchRequests,
+                                onLoadingChanged: (value) {
+                                  setState(() {
+                                    isLoadingConfirmation = value;
+                                    print(isLoadingConfirmation);
+                                  });
+                                },
                               ),
                             );
                           },
-                          child: RequestItem(
-                            request: filteredRequests[index],
-                            onRequestUpdated:
-                                fetchRequests, // Passing the update function
-                          ),
-                        );
-                      },
-                      childCount: filteredRequests.length,
-                    ),
-                  ),
+                          childCount: filteredRequests.length,
+                        ),
+                      ),
+              ],
+            ),
+            if (isLoading || isLoadingConfirmation)
+              Positioned.fill(
+                child: Container(
+                  child: Center(child: CircularProgressIndicator()),
+                ),
+              ),
+            if (isLoadingConfirmation)
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                  color: Colors.black.withOpacity(0.1),
+                ),
+              ),
           ],
         ),
       ),
@@ -277,10 +314,14 @@ class _InboxLecturerState extends State<InboxLecturer> {
 
 class RequestItem extends StatefulWidget {
   final Request request;
-  final Future<void> Function() onRequestUpdated; // Function to update requests
+  final Future<void> Function() onRequestUpdated;
+  final Function(bool) onLoadingChanged;
 
   const RequestItem(
-      {super.key, required this.request, required this.onRequestUpdated});
+      {super.key,
+      required this.request,
+      required this.onRequestUpdated,
+      required this.onLoadingChanged});
 
   @override
   State<RequestItem> createState() => _RequestItemState();
@@ -288,33 +329,6 @@ class RequestItem extends StatefulWidget {
 
 class _RequestItemState extends State<RequestItem> {
   bool isLoading = false;
-
-  Future<void> fetchRequests() async {
-    setState(() {
-      isLoading = true;
-    });
-    String url = Provider.of<ApiUrlProvider>(context, listen: false).baseUrl;
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    String userId = pref.getString('userId') ?? '';
-    final response = await http.get(Uri.parse('$url/requestMember/$userId'));
-    if (response.statusCode == 200) {
-      final List projectsJson = json.decode(response.body);
-      if (mounted) {
-        setState(() {
-          originalRequests = projectsJson.expand((projectJson) {
-            return (projectJson['Requests'] as List).map((requestJson) {
-              return Request.fromJson({...requestJson, 'project': projectJson});
-            });
-          }).toList();
-          filteredRequests = originalRequests;
-
-          isLoading = false;
-        });
-      }
-    } else {
-      throw Exception('Failed to load requests');
-    }
-  }
 
   void decline(int requestID) async {
     bool confirmed = await showDialog(
@@ -342,11 +356,12 @@ class _RequestItemState extends State<RequestItem> {
     );
 
     if (confirmed) {
-      if (mounted) {
-        setState(() {
-          isLoading = true;
-        });
-      }
+      if (!mounted) return;
+      setState(() {
+        isLoading = true;
+        widget.onLoadingChanged(true);
+        print("masuk");
+      });
 
       String url = Provider.of<ApiUrlProvider>(context, listen: false).baseUrl;
       final response = await http.patch(
@@ -355,22 +370,24 @@ class _RequestItemState extends State<RequestItem> {
         body: jsonEncode({'status': 'rejected'}),
       );
 
-      await widget.onRequestUpdated(); // Call the update function
-
       if (response.statusCode == 200) {
-        if (mounted) {
-          setState(() {
-            isLoading = false;
-          });
-        }
+        widget.onRequestUpdated();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content:
+                Text('Berhasil decline request ${widget.request.firstName}'),
+          ),
+        );
       } else {
-        if (mounted) {
-          setState(() {
-            isLoading = false;
-          });
-        }
-
         throw Exception('Failed to decline request');
+      }
+
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+          widget.onLoadingChanged(false);
+          print("masuk $isLoading ${widget.onLoadingChanged}");
+        });
       }
     }
   }
@@ -401,11 +418,11 @@ class _RequestItemState extends State<RequestItem> {
     );
 
     if (confirmed) {
-      if (mounted) {
-        setState(() {
-          isLoading = true;
-        });
-      }
+      if (!mounted) return;
+      setState(() {
+        isLoading = true;
+        widget.onLoadingChanged(true);
+      });
 
       String url = Provider.of<ApiUrlProvider>(context, listen: false).baseUrl;
       final response = await http.patch(
@@ -414,21 +431,35 @@ class _RequestItemState extends State<RequestItem> {
         body: jsonEncode({'status': 'accepted'}),
       );
 
-      await widget.onRequestUpdated(); // Call the update function
+      await http.post(
+        Uri.parse('$url/addNewMEM'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'projectID': widget.request.projectID,
+          'userID': widget.request.userID,
+          'roleID': widget.request.roleID
+        }),
+      );
+
+      if (!mounted) return;
 
       if (response.statusCode == 200) {
-        if (mounted) {
-          setState(() {
-            isLoading = false;
-          });
-        }
+        widget.onRequestUpdated();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content:
+                Text('Berhasil approve request ${widget.request.firstName}'),
+          ),
+        );
       } else {
-        if (mounted) {
-          setState(() {
-            isLoading = false;
-          });
-          throw Exception('Failed to approve request');
-        }
+        throw Exception('Failed to approve request');
+      }
+
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+          widget.onLoadingChanged(false);
+        });
       }
     }
   }
@@ -531,12 +562,6 @@ class _RequestItemState extends State<RequestItem> {
               ),
             ),
           ),
-          if (isLoading)
-            Positioned.fill(
-              child: Container(
-                child: Center(child: CircularProgressIndicator()),
-              ),
-            ),
         ],
       ),
     );
